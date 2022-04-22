48d7263185825d2250b19a13b5a6536f167e0f32
Garragames <96148865+garragames@users.noreply.github.com>
2022-04-22 18:42:17 -0500

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,37 @@
 namespace garragames {
-    let pms5003SensorPin: AnalogPin;
+    let pms5003SensorPin: DigitalPin;
     /**
-     Returns the value of the PMS5003 sensor on a scale of 0 to 100.
+     Returns the value of the PMS5003 sensor on a scale of 0 to 1000.
      */
-    //% blockId=octopus_moisture weight=10 blockGap=22
-    //% block="value of moisture sensor at pin %p"
-    export function MoistureSensor(p: AnalogPin): number {
-        return pins.map(pins.analogReadPin(p), 0, 950, 0, 100);
+    //% blockId=psm5003 weight=10 blockGap=22
+    //% block="value of particle sensor at pin %p"
+    export function psm5003Sensor(p: SerialPin): void {
+//        serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), () => {
+        let data = '';        
+        serial.onDataReceived(String.fromCharCode(0x4d), () => {
+            //basic.showString("READ");
+            data = serial.readUntil(String.fromCharCode(0x42));
+
+            console.log(data);
+            basic.showString(data, 100);
+            led.plot(0, 0);
+            basic.pause(100);
+            led.unplot(0, 0);
+        });
+        /*
+        let out = 0;
+        out = pins.digitalReadPin(p);
+        //console.log(out);
+        basic.showNumber(out);
+        return out;
+
+        */
+        return;
+    }
+
+    //% block="init sensor at pin %p"
+    export function psm5003InitSensor (p: SerialPin): void {
+        serial.redirect(p, p, BaudRate.BaudRate115200);
+        return;
     }
 }
