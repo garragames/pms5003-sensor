92689f925ba522d85d31adb6367d5962caed94a0
Garragames <96148865+garragames@users.noreply.github.com>
2022-04-27 16:04:39 -0500

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 namespace garragames {
+    let list = [];
+    list.push(6);
     let pms5003SensorPin: DigitalPin;
     /**
      Returns the value of the PMS5003 sensor on a scale of 0 to 1000.
@@ -6,16 +8,15 @@ namespace garragames {
     //% blockId=psm5003 weight=10 blockGap=22
     //% block="value of particle sensor at pin %p"
     export function psm5003Sensor(p: DigitalPin): void {
-        let data = '';        
+
+        let data = "";        
         serial.onDataReceived(String.fromCharCode(0x4d), () => {
-            //basic.showString("READ");
-            data = serial.readUntil(String.fromCharCode(0x42));
-      
-            // data = 'TEST';
-            //let num = Math.randomRange(1, 100);
-            let num = data.length;
-            basic.showNumber(num, 200);
- 
+            data = serial.readUntil(String.fromCharCode(0x42));      
+            for (let i = 0; i < data.length; i++) {
+                basic.showNumber(data.charCodeAt(i));
+                basic.showString(' - ');
+            }    
+            basic.showString(' > ');
             led.plot(0, 0);
             basic.pause(100);
             led.unplot(0, 0);
@@ -23,8 +24,7 @@ namespace garragames {
 
         led.plot(0, 5);
         basic.pause(100);
-        led.unplot(0, 5);
-        
+        led.unplot(0, 5);        
         return;
     }
 
--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-
-let particles = 0;
-while (true) {
-    garragames.psm5003Sensor(16);
-    basic.showNumber(particles, 150);
-    basic.pause(1000);
-}
