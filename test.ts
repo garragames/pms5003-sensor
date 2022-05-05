85a23e738de68877fbd5b04a6d9eb47fe4681574
Garragames <96148865+garragames@users.noreply.github.com>
2022-05-05 16:01:35 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 garragames.pms5003InitSensor(SerialPin.P16)
 basic.forever(function () {
-    basic.showNumber(garragames.pms5003Sensor(Metric.apm10))
+    basic.showNumber(garragames.pms5003Sensor(Metric.Apm10))
     basic.pause(1000)
 })
