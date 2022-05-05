d612ccbce46eac5ff16e2081f2e4d94d86b749f9
Garragames <96148865+garragames@users.noreply.github.com>
2022-05-05 14:34:57 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,32 @@
+enum Metric {
+    apm10 = 0,
+    apm25 = 1,
+    apm100 = 2,
+    pm10 = 4,
+    pm25 = 5,
+    pm100 = 6,
+    gt03 = 7,
+    gt05 = 8,
+    gt10 = 9,
+    gt25 = 10,
+    gt50 = 11,
+    gt100 = 12
+}
+
+let BUFF_SIZE = 32
+
+let Data = [BUFF_SIZE];
+
+let buffer: Buffer = pins.createBuffer(BUFF_SIZE);
+
 namespace garragames {
-    let apm10 = 255;
-
-    const enum Metric {
-        apm10 = 0,
-        apm25 = 1,
-        apm100 = 2
-    }
-    
-    /**
-     Returns the value of the PMS5003 sensor on a scale of 0 to 1000.
-     */
-    //% blockId=psm5003 weight=10 blockGap=22
-    //% block="value of particle sensor at pin %p"
-    export function psm5003Sensor(p: DigitalPin): void {
-
-    }
-
     /**
     Returns the value of the moisture sensor on a scale of 0 to 100.
     */
     //% blockId=psm5003B weight=10 blockGap=22
-    //% block="value of metric %m"
-    export function psm5003Sensor2(m: Metric): number {
-        return apm10;
+    //% block="value of metric %p"
+    export function psm5003Sensor(p: Metric): number {
+        return Data[p];
     }
 
     //% block="init sensor at pin %p"
@@ -31,3 +35,25 @@ namespace garragames {
         return;
     }
 }
+
+basic.forever(function () {
+    buffer = serial.readBuffer(BUFF_SIZE);
+
+    Data[ 0] = buffer.getUint8( 4) * 255 + buffer.getUint8( 5); // PM1.0, CF=1 [micro g/m3]        | diameter:  1.00
+    Data[ 1] = buffer.getUint8( 6) * 255 + buffer.getUint8( 7); // PM2.5, CF=1 [micro g/m3]        | diameter:  2.50
+    Data[ 2] = buffer.getUint8( 8) * 255 + buffer.getUint8( 9); // PM10.  CF=1 [micro g/m3]        | diameter: 10.00
+
+    Data[ 3] = buffer.getUint8(10) * 255 + buffer.getUint8(11); // PM1.0 [micro g/m3]              | diameter:  1.00
+    Data[ 4] = buffer.getUint8(12) * 255 + buffer.getUint8(13); // PM2.5 [micro g/m3]              | diameter:  2.50
+    Data[ 5] = buffer.getUint8(14) * 255 + buffer.getUint8(15); // PM10. [micro g/m3]              | diameter: 10.00
+
+    Data[ 6] = buffer.getUint8(16) * 255 + buffer.getUint8(17); // Particles > 0.3 micron [/0.1L]  | diameter:  0.30
+    Data[ 7] = buffer.getUint8(18) * 255 + buffer.getUint8(19); // Particles > 0.5 micron [/0.1L]  | diameter:  0.50
+    Data[ 8] = buffer.getUint8(20) * 255 + buffer.getUint8(21); // Particles > 1.0 micron [/0.1L]  | diameter:  1.00
+    Data[ 9] = buffer.getUint8(22) * 255 + buffer.getUint8(23); // Particles > 2.5 micron [/0.1L]  | diameter:  2.50
+    Data[10] = buffer.getUint8(24) * 255 + buffer.getUint8(25); // Particles > 5.0 micron [/0.1L]  | diameter:  5.00
+    Data[11] = buffer.getUint8(26) * 255 + buffer.getUint8(27); // Particles > 10. micron [/0.1L]  | diameter: 10.00
+
+    Data[12] = buffer.getUint8(28) * 255 + buffer.getUint8(29); // Reserved_0 [???]                | diameter: nan
+    Data[13] = buffer.getUint8(30) * 255 + buffer.getUint8(31); // Reserved_0 [???]                | diameter: nan
+});
