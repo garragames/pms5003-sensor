477092f4ec1b0c95cc80f74ff8c45368fec14616
Garragames <96148865+garragames@users.noreply.github.com>
2022-05-05 15:34:44 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-garragames.psm5003InitSensor(SerialPin.P16)
+garragames.pms5003InitSensor(SerialPin.P16)
 basic.showString("APM 1.0")
 basic.forever(function () {
-    basic.showNumber(garragames.psm5003Sensor(Metric.apm10))
+    basic.showNumber(garragames.pms5003Sensor(Metric.apm10))
     basic.pause(1000)
 })
